895e997ad77a22eb03a2b9ebf765a436b00c9f90
jPursey-UoM <jpursey@student.unimelb.edu.au>
2018-04-20 12:33:33 +1000

    turns counter in State, State.shrink_board() works

--- state.py
+++ state.py
@@ -1,8 +1,8 @@
 class State:
     board = []
     # 2d array of chars representing board and pieces.
-    # blank: "-",  black: "@", white: "O"
-    turns = None  # int. total number of moves made by BOTH players so far
+    # blank: "-",  black: "@", white: "O", removed by dz: "#"
+    turns = None  # int. total number of turns made by BOTH players so far
     current_player = None  # int.  -1 = "white" or  1 = "black"
 
 
@@ -14,7 +14,9 @@ class State:
         self.board = board_list
 
     @staticmethod
-    def is_surrounded(x, y, board_array):
+    def is_surrounded(row, col, board_array):
+        x = col
+        y = row
         if(x<0 or y<0 or x>=len(board_array) or y>=len(board_array)) :
             return False
         centre = board_array[y][x]
@@ -39,14 +41,16 @@ class State:
                 return True
 
         return False
+
+
     # generate board based on parent and move
     @staticmethod
-    def generate(parent_b, move_m):
+    def generate(previous, move_m):
         # copy parent
 
-        array = []
-        for row in parent_b.array:
-            array.append(list(row))
+        board = []
+        for row in previous.board:
+            board.append(list(row))
 
         # move one piece based on move
         x1 = move_m.start_x
@@ -54,24 +58,31 @@ class State:
         x2 = move_m.end_x
         y2 = move_m.end_y
 
-        temp = array[y1][x1]
-        array[y1][x1] = array[y2][x2]
-        array[y2][x2] = temp
+        temp = board[y1][x1]
+        board[y1][x1] = board[y2][x2]
+        board[y2][x2] = temp
 
         # work out which (if any) pieces got taken
-        if(State.is_surrounded(x2 - 1, y2, array)) :
-            array[y2][x2-1]="-"
-        if (State.is_surrounded(x2 + 1, y2, array)):
-            array[y2][x2 + 1] = "-"
-        if (State.is_surrounded(x2, y2 - 1, array)) :
-            array[y2-1][x2]="-"
-        if (State.is_surrounded(x2, y2 + 1, array)):
-            array[y2+1][x2] = "-"
-        if(State.is_surrounded(x2, y2, array)) :
-            array[y2][x2]="-"
-        # create and return the Board object
+        if(State.is_surrounded(y2, x2 - 1, board)) :
+            State.clear_square(y2, x2 - 1, board)
+        if (State.is_surrounded(y2, x2 + 1, board)):
+            State.clear_square(y2, x2 + 1, board)
+        if (State.is_surrounded(y2 - 1, x2, board)) :
+            State.clear_square(y2 - 1, x2, board)
+        if (State.is_surrounded(y2 + 1, x2, board)):
+            State.clear_square(y2 + 1, x2, board)
+        if(State.is_surrounded(y2, x2, board)) :
+            State.clear_square(y2, x2, board)
 
-        return State(array)
+        # check if board needs to shrink
+        turns = previous.turns + 1
+        if turns == 128 + 24:  # 24 is number of turns in placing phase
+            board = State.shrink_board(board, 128)
+        if turns == 192 + 24:
+            board = State.shrink_board(board, 192)
+
+        # create and return the State object
+        return State(turns, -1*previous.current_player, board)
 
     def print_board(self):
         print("  01234567")
@@ -80,3 +91,54 @@ class State:
             print(str(i) + " " + "".join(line))
             i += 1
 
+    @staticmethod
+    def shrink_board(board, dz_type):
+        width = 0  # width of dead space padding
+        if dz_type == 128:
+            width = 1
+        if dz_type == 192:
+            width = 2
+
+        # update edges
+        for i in range(width - 1, len(board) - (width - 1)):
+            board[width - 1][i] = "#"
+            board[i][width - 1] = "#"
+            board[-width][i] = "#"
+            board[i][-width] = "#"
+
+        # update corners
+        board[width][width] = "X"
+        board[width][-(width + 1)] = "X"
+        board[- (width + 1)][width] = "X"
+        board[- (width + 1)][- (width + 1)] = "X"
+
+        # check if remaining pieces surrounded (outer edges of remaining space)
+        start = width
+        end = len(board) - width
+        iterator = range(start, end)
+
+        # down col 1
+        for i in iterator:
+            if State.is_surrounded(i, start, board):
+                State.clear_square(i, start, board)
+
+        # right along row 6
+        for i in iterator:
+            if State.is_surrounded(end - 1, i, board):
+                State.clear_square(end - 1, i, board)
+
+        # up col 6
+        for i in reversed(iterator):
+            if State.is_surrounded(i, end - 1, board):
+                State.clear_square(i, end - 1, board)
+
+        # left along row 1
+        for i in reversed(iterator):
+            if State.is_surrounded(start, i, board):
+                State.clear_square(start, i, board)
+
+        return board
+
+    @staticmethod
+    def clear_square(row, col, board):
+        board[row][col] = "-"
--- testmain.py
+++ testmain.py
@@ -1,5 +1,5 @@
 from state import *
-
+from Move import *
 
 def convert_board(board_list):
     # convert the input list of strings to a list of lists of chars,
@@ -31,5 +31,35 @@ def new_board():
 
 board = new_board()
 board[1][1] = "@"
-current = State(0, -1, board)
+board[1][2] = "@"
+board[1][3] = "O"
+board[3][3] = "O"
+
+board[4][1] = "@"
+board[5][1] = "O"  # should be surrounded by corner
+
+board[6][4] = "@"
+board[6][5] = "O"  # should be surrounded by corner
+
+board[1][4] = "@"
+board[1][5] = "O"  # should be surrounded by corner
+
+board[2][6] = "@"
+board[3][6] = "O"  # should be surrounded by corner
+
+
+current = State(127+24, -1, board)
 current.print_board()
+
+next_state = State.generate(current, Move(3,3,4,3));
+next_state.print_board()
+
+next_state.board[2][3] = "O"
+next_state.print_board()
+next_state = State.generate(next_state, Move(3,2,3,1))
+next_state.print_board()
+
+next_state.turns = 191 + 24
+another_state = State.generate(next_state, Move(4,6,4,5))
+another_state.print_board()
+
